f4ff1bce5299bbbb91da97c84b72e7e185076753
幹人 永澤 <230413@jc-21.jp>
2024-01-26 17:50:01 +0900

    完成

--- Enemy.cpp
+++ Enemy.cpp
@@ -18,14 +18,14 @@ void Enemy::Initialize()
 	assert(hModel_ >= 0);
 	Model::SetAnimFrame(hModel_, 0, 60, 1);
 	//transform_.position_ = { (float)(rand() % 41 - 20), 0, 30 };
-	//float x = (float)rand() / RAND_MAX;//0-1‚Ì—”
+	float x = (float)rand() / RAND_MAX;//0-1‚Ì—”
 
-	//x = 2.0 * x;//0-2‚Ì—”
-	//transform_.position_.x = 25.0 * (x - 1.0);
-	//float z = (float)rand() / RAND_MAX;//0-1‚Ì—”
-	//z = 2.0 * z;//0-2‚Ì—”
-	//transform_.position_.z = 25.0 * (z - 1.0);
-	//transform_.position_.y = 0;
+	x = 2.0 * x;//0-2‚Ì—”
+	transform_.position_.x = 25.0 * (x - 1.0);
+	float z = (float)rand() / RAND_MAX;//0-1‚Ì—”
+	z = 2.0 * z;//0-2‚Ì—”
+	transform_.position_.z = 25.0 * (z - 1.0);
+	transform_.position_.y = 0;
 
 	Ground* pGround = (Ground*)FindObject("Ground");
 	int hGmodel = pGround->GetModelHandle();
